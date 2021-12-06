d358e10b4e8795e9d1a6f6fa154d9472a43de462
alex <alex@slab.org>
2021-12-06 19:56:37 +0000

    fix

--- py/play.py
+++ py/play.py
@@ -8,8 +8,8 @@ lib_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + '/lib/'
 sys.path.insert(0, lib_dir)
 
 import link
-import vortex
 
+from vortex import *
 
 l = link.Link(135)
 
@@ -21,24 +21,34 @@ ticks = 0
 frame = (1/20) * 1000000
 bpc = 4
 
+pattern = sound(pure("bd"))
+
 try:
     while True:
         ticks = ticks + 1
         
         logical_now = math.floor(start + (ticks * frame))
+        logical_next = math.floor(start + ((ticks + 1) * frame))
+
         # wait until start of next frame
         wait = (logical_now - l.clock().micros())/1000000;
         if wait > 0:
             time.sleep(wait)
 
         s = l.captureSessionState()
-        cycle = s.beatAtTime(logical_now, 0) / bpc
+        cycle_from = s.beatAtTime(logical_now, 0) / bpc
+        cycle_to = s.beatAtTime(logical_next, 0) / bpc
+        es = pattern.onsetsOnly().query(TimeSpan(Time(cycle_from),Time(cycle_to)))
+        if len(es):
+            print("\n" + str([e.value for e in es]))
+        
         cps = (s.tempo() / bpc) / 60
 
         sys.stdout.write("cps %.2f | playing %s | cycle %.2f\r"
-                         % (cps, s.isPlaying(), cycle))
+                         % (cps, s.isPlaying(), cycle_from))
         
         sys.stdout.flush()
 
+        
 except KeyboardInterrupt:
     pass
