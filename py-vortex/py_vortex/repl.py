9e0178f605db82debd9ad66cdf1adb9524eec6b7
Damián Silvani <munshkr@gmail.com>
2021-12-09 19:28:51 -0300

    Define a `p` function for creating streams on REPL

--- py-vortex/py_vortex/repl.py
+++ py-vortex/py_vortex/repl.py
@@ -1,12 +1,28 @@
 from IPython.terminal.embed import InteractiveShellEmbed
 
 from py_vortex import *
+from py_vortex.stream import LinkClock, SuperDirtStream
 from py_vortex import __version__
 
 
-def start_repl():
+def start_repl(*kwargs):
     # Every binding defined here will be available on the REPL...
-    foo = "bar"
+    default_clock = LinkClock(bpm=120)
+    default_clock.start()
 
+    # Define a few useful functions...
+    _patterns = {}
+
+    def p(key, pattern):
+        if key not in _patterns:
+            stream = SuperDirtStream(*kwargs)
+            default_clock.subscribe(stream)
+            _patterns[key] = stream
+        _patterns[key].pattern = pattern
+        return pattern
+
+    # Start the interactive shell
     ipshell = InteractiveShellEmbed(banner1=f"Vortex {__version__}")
     ipshell()
+
+    default_clock.stop()
