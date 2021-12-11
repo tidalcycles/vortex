40822d27ab6f03249839f6f4a3817ba5efb46711
Damián Silvani <munshkr@gmail.com>
2021-12-11 09:07:44 -0300

    Replace tkinter editor for a QScintilla basic editor

--- py-vortex/py_vortex/gui.py
+++ py-vortex/py_vortex/gui.py
@@ -1,51 +1,72 @@
-import tkinter as tk
-import tkinter.font
-from tkinter import ttk
+import sys
 
-from py_vortex.stream import vortex_dsl
+from PyQt5.Qsci import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from py_vortex import vortex_dsl
 
 
-class App(tk.Tk):
-    def __init__(self, vortex_module):
-        super().__init__()
+class VortexMainWindow(QMainWindow):
+    def __init__(self, dsl_module):
+        super(VortexMainWindow, self).__init__()
 
-        self._vortex_module = vortex_module
+        self._dsl_module = dsl_module
 
-        # Root window
-        self.title("Vortex")
-        self.style = ttk.Style(self)
+        # Define the geometry of the main window
+        self.setGeometry(300, 300, 800, 400)
+        self.setWindowTitle("Vortex")
 
-        scrollbar = tk.Scrollbar(self)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        # Create frame and layout
+        self._frame = QFrame(self)
+        self._frame.setStyleSheet("QWidget { background-color: #ffeaeaea }")
+        self._layout = QVBoxLayout()
+        self._frame.setLayout(self._layout)
+        self.setCentralWidget(self._frame)
 
-        self.document = tk.Text(self, yscrollcommand=scrollbar.set, padx=5, pady=5)
-        self.document.pack(expand=True, fill=tk.BOTH)
+        self._editorFont = QFont()
+        self._editorFont.setPointSize(16)
+        self._editorFont.setFamily("monospace")
 
-        font = tkinter.font.Font(family="monospace", size=14)
-        self.document.configure(
-            font=font,
-            blockcursor=True,
-            insertbackground="white",
-            bg="black",
-            fg="white",
-        )
+        # Create and configure Editor
+        self._editor = QsciScintilla()
+        self._editor.setUtf8(True)  # Set encoding to UTF-8
+        self._editor.setIndentationGuides(True)
+        self._editor.setCaretLineVisible(True)
+        self._editor.setCaretLineBackgroundColor(QColor("#1fff0000"))
+        self._editor.setCaretWidth(3)
 
-        scrollbar.config(command=self.document.yview)
+        # Create Python Lexer
+        self._lexer = QsciLexerPython(self._editor)
+        self._lexer.setFont(self._editorFont)
+        self._editor.setLexer(self._lexer)
 
-        self.document.bind("<Control-Return>", self.evaluate_block)
-        # self.bind_all("<Escape>", lambda e: self.destroy())
+        # Commands and shortcuts
+        commands = self._editor.standardCommands()
+        command = commands.boundTo(Qt.ControlModifier | Qt.Key_Return)
+        # Clear the default
+        if command is not None:
+            command.setKey(0)
+        shortcut = QShortcut(Qt.ControlModifier | Qt.Key_Return, self._editor)
+        shortcut.activated.connect(self.evaluate_block)
 
-        self.document.focus()
+        # Add editor to layout
+        self._layout.addWidget(self._editor)
 
-    def evaluate_block(self, event=None):
+        self.show()
+
+    def evaluate_block(self):
         print("TODO: Evaluate code")
         code = "hush()"
         print(f"Eval: '{code}'")
-        exec(code, vars(self._vortex_module))
-        return "break"
+        exec(code, vars(self._dsl_module))
 
 
 def start_gui():
+    app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create("Fusion"))
+
     with vortex_dsl() as module:
-        app = App(vortex_module=module)
-        app.mainloop()
+        myGUI = VortexMainWindow(dsl_module=module)
+        app.exec_()
