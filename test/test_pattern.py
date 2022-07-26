06a7c7bf8006c806370a820bb9528d7089703048
Damián Silvani <munshkr@gmail.com>
2022-07-26 09:07:40 -0300

    feat: `layer`

--- test/test_pattern.py
+++ test/test_pattern.py
@@ -1,15 +1,23 @@
-from vortex.pattern import TimeSpan, Fraction, pure, stack
+from vortex.pattern import TimeSpan, Fraction, pure, stack, fastcat, rev
 
 
 def assert_equal_patterns(input, expected, span=None):
     """Assert patterns by queyring them and comparing events"""
     if not span:
         span = TimeSpan(Fraction(0), Fraction(1))
-    assert input.query(span) == expected.query(span)
+    assert sorted(input.query(span)) == sorted(expected.query(span))
 
 
 def test_superimpose():
     assert_equal_patterns(
         pure("bd").superimpose(lambda p: p.fast(3)),
         stack(pure("bd"), pure("bd").fast(3)),
+    )
+
+
+def test_layer():
+    basepat = fastcat(pure("bd"), pure("sd"))
+    assert_equal_patterns(
+        basepat.layer(rev, lambda p: p.fast(2)),
+        stack(basepat.rev(), basepat.fast(2)),
     )
