0a6291f8c368fd5244787ef5718d05ec47b7df00
Damián Silvani <munshkr@gmail.com>
2022-07-31 11:54:59 -0300

    Get Vortex version from package metadata using `pkg_resources` module

--- vortex/repl.py
+++ vortex/repl.py
@@ -7,5 +7,5 @@ from vortex import __version__
 def start_repl(*kwargs):
     """Start the interactive shell inside a Vortex environment"""
     with vortex_dsl() as module:
-        ipshell = InteractiveShellEmbed(banner1=f"Vortex {__version__}")
+        ipshell = InteractiveShellEmbed(banner1=f"Vortex REPL, version {__version__}")
         ipshell(module=module)
